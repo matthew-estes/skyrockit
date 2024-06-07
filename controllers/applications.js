6dd5a78327cdb8d04b8b4e6555e32998a3d44b46
matthew-estes <matthew.estes19@gmail.com>
2024-06-07 17:18:55 -0400

    completed...css work needed

---
+++ controllers/applications.js
@@ -0,0 +1,94 @@
+const User = require("../models/user");
+
+async function index(req, res) {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
+  } catch (err) {
+    res.redirect("/");
+  }
+}
+
+function newAppForm(req, res) {
+  res.render("applications/new.ejs");
+}
+
+async function create(req, res) {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+}
+
+async function show(req, res) {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render("applications/show.ejs", { application, user: currentUser });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+}
+
+async function edit(req, res) {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render("applications/edit.ejs", {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+}
+
+async function update(req, res) {
+  try {
+    // Find the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the current application from the id supplied by req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Use the Mongoose .set() method, updating the current application to reflect the new form data on `req.body`
+    application.set(req.body);
+    // Save the current user
+    await currentUser.save();
+    // Redirect back to the show view of the current application
+    res.redirect(
+      `/users/${currentUser._id}/applications/${req.params.applicationId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+}
+
+async function deleteApp(req, res) {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+}
+
+module.exports = {
+  index,
+  new: newAppForm,
+  create,
+  show,
+  delete: deleteApp,
+  edit,
+  update,
+};
